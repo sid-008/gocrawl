3744f2b253127b23d7b6ab499446b5e7a9faa308
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-04-15 14:09:00 +0530

    Feat: scrape links from single site

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sid-008/gocrawl
 
 go 1.20
+
+require golang.org/x/net v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+type Page struct {
+	url  string
+	html string
+}
+
+func fetch(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close() // need to make sure there are no resource leaks
+	log.Println("STATUS CODE:", res.StatusCode)
+
+	content, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	page := &Page{
+		url:  url,
+		html: string(content),
+	}
+
+	re := regexp.MustCompile(`href=["']([^"']+)["']`)
+	body := page.html
+	links := re.FindAllStringSubmatch(body, -1)
+	for _, link := range links {
+		fmt.Println(link[1])
+	}
+}
 
 func main() {
-	fmt.Println("hello!")
+	fetch("https://www.google.com")
 }
