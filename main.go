0db24ea3a47e46245a34174ab2a87a2b9d215c61
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-04-15 23:12:43 +0530

    Feat: multithreaded now

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
 
 	"github.com/sid-008/gocrawl/queue"
 )
@@ -16,8 +17,10 @@ type Page struct {
 	html string
 }
 
-func fetch(url string, queue *queue.Queue) {
-	x := strings.HasPrefix(url, "https")
+func fetch(url string, queue *queue.Queue, wg *sync.WaitGroup, urls chan string) {
+	defer wg.Done()
+	//log.Println(count)
+	x := strings.HasPrefix(url, "http")
 	if !x {
 		return
 	}
@@ -27,7 +30,7 @@ func fetch(url string, queue *queue.Queue) {
 		log.Println(err)
 	}
 	defer res.Body.Close() // need to make sure there are no resource leaks
-	log.Println("STATUS CODE:", res.StatusCode)
+	//log.Println("STATUS CODE for", url, ":", res.StatusCode) //this was for logging purposes
 
 	content, err := io.ReadAll(res.Body)
 	if err != nil {
@@ -38,27 +41,51 @@ func fetch(url string, queue *queue.Queue) {
 		html: string(content),
 	}
 
-	findLinks(page, queue)
+	findLinks(page, queue, wg, urls)
 }
 
-func findLinks(page *Page, queue *queue.Queue) {
+func findLinks(page *Page, queue *queue.Queue, wg *sync.WaitGroup, urls chan string) {
+	defer wg.Done()
 	re := regexp.MustCompile(`href=["']([^"']+)["']`)
 	body := page.html
 	links := re.FindAllStringSubmatch(body, -1)
 	for _, link := range links {
-		fmt.Println(link[1])
 		queue.Enqueue(link[1])
+		urls <- link[1]
 	}
 }
 
+func consume(urls chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for link := range urls {
+		fmt.Println(link)
+	}
+}
+
+var urls chan string
+
+// var count uint64
+
+//TODO currently the program runs indefinitely and throws an OOM error, certain memory optimisations have to be made. For instance it could be written to a disk or a db rather than any sort of in memory storage
+//TODO the OOM error issue rn is probably the most severe one that interferes with the crawler functioning
+
 func main() {
-	initialUrl := "https://google.com"
+	wg := &sync.WaitGroup{}
+	urls = make(chan string, 100)
+	initialUrl := "https://linuxhint.com"
 	queue := &queue.Queue{}
 	queue.Enqueue(initialUrl)
 
 	for {
+		/*count++
+		if count > 100000 {
+			break
+		}*/
 		next := queue.Dequeue()
-		fmt.Println("The next url to be explored is:\n\n\n", next)
-		fetch(next, queue)
+		wg.Add(1)
+		go fetch(next, queue, wg, urls)
+		wg.Add(1)
+		go consume(urls, wg)
 	}
+	// log.Println("count is:", count)
 }
